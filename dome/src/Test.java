426a0233a50b34547b072b5bccfae31b1bda26e6
李勇 <liyong1@supermap.com>
2018-11-23 15:55:05 +0800

    Initial commit

---
+++ dome/src/Test.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+/**
+ * @author highsad
+ */
+public class Test {
+	private JButton button1;
+	private JPanel panel;
+
+/*	public static void main(String[] args) {
+		JFrame frame = new JFrame("这是一个计算器");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300, 100);
+		frame.setVisible(true);
+	}*/
+}
